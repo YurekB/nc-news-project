4e3b642f2315fd954c7f56bd4e7d432e07388d1d
YurekB <boguckiyurek@gmail.com>
2022-08-01 16:52:25 +0100

    changed test slightly

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -68,7 +68,7 @@ describe("GET Requests", () => {
             expect.objectContaining({
               author: expect.any(String),
               title: expect.any(String),
-              article_id: expect.any(Number),
+              article_id: 1,
               body: expect.any(String),
               topic: expect.any(String),
               created_at: expect.any(String),
