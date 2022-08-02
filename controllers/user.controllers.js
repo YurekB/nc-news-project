eb1372f5eeb6f3f9ed631eead5a245e5a2d66fe8
YurekB <boguckiyurek@gmail.com>
2022-08-02 18:12:50 +0100

    added a catch onto the controller

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,7 +1,11 @@
 const { fetchUsers } = require("../models/user.models");
 
 exports.getUsers = (req, res, next) => {
-  fetchUsers().then((response) => {
-    res.status(200).send({ users: response });
-  });
+  fetchUsers()
+    .then((response) => {
+      res.status(200).send({ users: response });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
