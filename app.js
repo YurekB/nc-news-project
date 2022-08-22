9764b4be72c96f00dbcd6f102ebcfbc26c1a200d
YurekB <boguckiyurek@gmail.com>
2022-08-22 22:24:28 +0100

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const cors = require("cors");
+
 const express = require("express");
 
 const apiRouter = require("./routers/api-routers");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
